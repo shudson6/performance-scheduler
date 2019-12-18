1a910d771afbcef556457a5b8adeee1a6a27f61c
Steven Hudson <shudson6@asu.edu>
2019-12-18 09:22:49 -0500

    Changed Storage to throw IOException from all methods. This is a general enough exception to be acceptable by all implementors while informing clients of problems

--- src/main/performancescheduler/data/storage/Storage.java
+++ src/main/performancescheduler/data/storage/Storage.java
@@ -1,5 +1,6 @@
 package performancescheduler.data.storage;
 
+import java.io.IOException;
 import java.util.Collection;
 
 import performancescheduler.data.Feature;
@@ -12,17 +13,20 @@ public interface Storage {
     /**
      * Load in the Features.
      * @return a new {@code Collection} of {@code Feature}s
+     * @throws IOException in event of failure
      */
-    public Collection<Feature> loadFeatureData();
+    public Collection<Feature> loadFeatureData() throws IOException;
     /**
      * Load in the Performances.
      * @return a new {@code Collection} of {@code Performance}s
+     * @throws IOException in event of failure
      */
-    public Collection<Performance> loadPerformanceData();
+    public Collection<Performance> loadPerformanceData() throws IOException;
     /**
      * Save what you've been working on. Whether or not either parameter may be null is up to the implementation. 
      * @param featureData the features to save
      * @param performanceData the performances to save
+     * @throws IOException in event of failure
      */
-    public void save(Collection<Feature> featureData, Collection<Performance> performanceData);
+    public void save(Collection<Feature> featureData, Collection<Performance> performanceData) throws IOException;
 }
--- src/main/performancescheduler/data/storage/xml/XmlStorage.java
+++ src/main/performancescheduler/data/storage/xml/XmlStorage.java
@@ -1,17 +1,28 @@
 package performancescheduler.data.storage.xml;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Collection;
+
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLStreamException;
 
 import performancescheduler.data.Feature;
 import performancescheduler.data.Performance;
 import performancescheduler.data.storage.Storage;
 
 public class XmlStorage implements Storage {
+    private File file;
+    private XmlLoader loader;
+    private Collection<Feature> features = null;
+    private Collection<Performance> performances = null;
 
     @Override
-    public Collection<Feature> loadFeatureData() {
-        // TODO Auto-generated method stub
-        return null;
+    public Collection<Feature> loadFeatureData() throws IOException {
+        if (features == null) {
+            load();
+        }
     }
 
     @Override
@@ -26,4 +37,19 @@ public class XmlStorage implements Storage {
         
     }
 
+    private void load() throws IOException {
+        try {
+            loader.load(features, performances);
+        } catch (FileNotFoundException e) {
+            rethrow(e, "Could not find the file " + file.getAbsolutePath());
+        } catch (XMLStreamException e) {
+            rethrow(e, "Error while parsing " + file.getAbsolutePath());
+        } catch (FactoryConfigurationError e) {
+            rethrow(e, "FactoryConfigurationError thrown by XmlLoader");
+        }
+    }
+    
+    private void rethrow(Throwable cause, String message) throws IOException {
+        throw new IOException(message, cause);
+    }
 }
