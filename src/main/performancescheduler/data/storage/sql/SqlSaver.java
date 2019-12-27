60df9b417aa0ca7ad99bb7b1f017d7e6cddb1ecb
Steven Hudson <shudson6@asu.edu>
2019-12-26 23:27:37 -0500

    broke out SqlSaver; ready for test

--- src/main/performancescheduler/data/storage/sql/SqlSaver.java
+++ src/main/performancescheduler/data/storage/sql/SqlSaver.java
@@ -1,16 +1,49 @@
 package performancescheduler.data.storage.sql;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.Objects;
 
-import performancescheduler.data.Feature;
-import performancescheduler.data.Performance;
 import performancescheduler.data.storage.MetaFeature;
 import performancescheduler.data.storage.MetaPerformance;
 
-abstract class SqlSaver {
-    private Collection<MetaFeature> mfins = new ArrayList<>();
-    private Collection<MetaFeature> mfdel = new ArrayList<>();
-    private Collection<MetaPerformance> mpins = new ArrayList<>();
-    private Collection<MetaPerformance> mpdel = new ArrayList<>();
+class SqlSaver {
+    private final DbConnectionService dcs;
+    
+    public SqlSaver(DbConnectionService connection) {
+        Objects.requireNonNull(connection);
+        dcs = connection;
+    }
+    
+    public void save(Collection<MetaFeature> features, Collection<MetaPerformance> performances) throws IOException {
+        PsqlDeactivateBuilder delete = new PsqlDeactivateBuilder();
+        // do performances first, since they have dependencies (don't assume ON DELETE CASCADE)
+        PsqlInsertPerformanceBuilder pInsert = new PsqlInsertPerformanceBuilder();
+        if (performances != null) {
+            performances.stream().filter(p -> p.getWrapped() != null).forEach(pInsert::add);
+            performances.stream().filter(p -> p.getWrapped() == null).forEach(delete::add);
+        }
+        PsqlInsertBuilder<MetaFeature> fInsert = new PsqlInsertBuilder<>(new FeatureValueLister(), SQL.TBL_FEATURE);
+        if (features != null) {
+            features.stream().filter(mf -> mf.getWrapped() != null).forEach(fInsert::add);
+            features.stream().filter(mf -> mf.getWrapped() == null).forEach(delete::add);
+        }
+        try {
+            String cmd = delete.getCommand();
+            if (!cmd.isEmpty()) {
+                dcs.getStatement().execute(cmd);
+            }
+            cmd = pInsert.getCommand();
+            if (!cmd.isEmpty()) {
+                dcs.getStatement().execute(cmd);
+            }
+            cmd = fInsert.getCommand();
+            if (!cmd.isEmpty()) {
+                dcs.getStatement().execute(cmd);
+            }
+        } catch (IOException | SQLException ex) {
+            throw new IOException("Exception occurred while saving data.", ex);
+        }
+    }
 }
--- src/main/performancescheduler/data/storage/sql/SqlStorage.java
+++ src/main/performancescheduler/data/storage/sql/SqlStorage.java
@@ -1,71 +1,21 @@
 package performancescheduler.data.storage.sql;
 
 import java.io.IOException;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Collection;
 
-import performancescheduler.data.Feature;
-import performancescheduler.data.Performance;
 import performancescheduler.data.storage.MetaFeature;
 import performancescheduler.data.storage.MetaPerformance;
-import performancescheduler.data.storage.MetaWrapper;
-import performancescheduler.data.storage.Storage;
 
-public class SqlStorage implements Storage {
-    protected final DbConnectionService dcs;
-    
-    public SqlStorage() {
-        dcs = new DbConnectionService();
-    }
+public class SqlStorage {
+    protected DbConnectionService dcs;
 
-    @Override
-    public void restore(Collection<Feature> featureData, Collection<Performance> performanceData) throws IOException {
+    public void restore(Collection<MetaFeature> featureData, Collection<MetaPerformance> performanceData)
+            throws IOException {
         
     }
 
-    @Override
-    public void store(Collection<Feature> featureData, Collection<Performance> performanceData) throws IOException {
-        insertFeatures(featureData);
-        insertPerformances(performanceData);
-    }
-    
-    private void insertFeatures(Collection<Feature> featureData) throws IOException {
-        PsqlInsertBuilder<MetaFeature> mfInsert = new PsqlInsertBuilder<>(new FeatureValueLister(), SQL.TBL_FEATURE);
-        mfInsert.addAll(downCastCollection(featureData, new ArrayList<MetaFeature>()));
-        try {
-            statement().execute(mfInsert.getCommand());
-        } catch (SQLException e) {
-            throw new IOException("Failed to insert feature data: see cause", e);
-        }
-    }
-    
-    private void insertPerformances(Collection<Performance> pData) throws IOException {
-        PsqlInsertPerformanceBuilder mpInsert = new PsqlInsertPerformanceBuilder();
-        mpInsert.addAll(downCastCollection(pData, new ArrayList<MetaPerformance>()));
-        try {
-            statement().execute(mpInsert.getCommand());
-        } catch (SQLException e) {
-            throw new IOException("Failed to insert performance data: see cause", e);
-        }
-    }
-
-    private Statement statement() throws IOException {
-        if (dcs.isStarted()) {
-            return dcs.getStatement();
-        } else {
-            throw new IOException("Database connection not started.");
-        }
-    }
-    
-    @SuppressWarnings("unchecked")
-    private <T, E extends MetaWrapper<T>> Collection<E> downCastCollection(Collection<T> coll, Collection<E> cast) {
-        for (T t : coll) {
-            if (t instanceof MetaWrapper<?>) {
-                cast.add((E) t);
-            }
-        }
-        return cast;
+    public void store(Collection<MetaFeature> featureData, Collection<MetaPerformance> performanceData) 
+            throws IOException {
+        new SqlSaver(dcs).save(featureData, performanceData);
     }
 }
