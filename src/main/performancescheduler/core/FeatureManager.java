188e96c74cf5875214af1cd0e8a2ccccc5df6c6f
Steven Hudson <steven.g.hudson@gmail.com>
2019-12-28 22:38:53 -0500

    FeatureManager ready for test; ScheduleEvent test needs update

--- src/main/performancescheduler/core/FeatureManager.java
+++ src/main/performancescheduler/core/FeatureManager.java
@@ -1,7 +1,11 @@
 package performancescheduler.core;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.TreeSet;
 
 import performancescheduler.core.event.EventFactory;
 import performancescheduler.core.event.FeatureDataListener;
@@ -12,6 +16,16 @@ public class FeatureManager {
 	private List<FeatureDataListener> listenerList;
 	private EventFactory eventFactory = EventFactory.newFactory();
 	private boolean eventsEnabled = true;
+	
+	private Collection<Feature> featureData = new TreeSet<>();
+	
+	public boolean add(Feature toAdd) {
+		boolean result = featureData.add(toAdd);
+		if (result) {
+			fireAddFeature(toAdd);
+		}
+		return result;
+	}
 	
 	public void addFeatureDataListener(FeatureDataListener listener) {
 		if (!listenerList.contains(listener)) {
@@ -47,11 +61,42 @@ public class FeatureManager {
 	    }
 	}
 	
+	public Collection<Feature> getData() {
+		return new ArrayList<>(featureData);
+	}
+	
+	public boolean remove(Feature toRemove) {
+		boolean result = featureData.remove(toRemove);
+		if (result) {
+			fireRemoveFeature(toRemove);
+		}
+		return result;
+	}
+	
 	public void removeFeatureDataListener(FeatureDataListener listener) {
 		listenerList.remove(listener);
 	}
 	
+	public void setData(Collection<Feature> data) {
+		Objects.requireNonNull(data);
+		Collection<Feature> old = featureData;
+		featureData = new TreeSet<>(data);
+		if (eventsEnabled) {
+			fireEvent(eventFactory.newRemoveFeatureEvent(old));
+			fireEvent(eventFactory.newAddFeatureEvent(data));
+		}
+	}
+	
 	public void setEventsEnabled(boolean enable) {
 		eventsEnabled = enable;
+	}
+	
+	public boolean update(Feature before, Feature after) {
+		if (featureData.remove(before)) {
+			featureData.add(after);
+			fireUpdateEvent(after, before);
+			return true;
+		}
+		return false;
 	}
 }
--- src/main/performancescheduler/core/event/ScheduleEvent.java
+++ src/main/performancescheduler/core/event/ScheduleEvent.java
@@ -3,6 +3,7 @@ package performancescheduler.core.event;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 public class ScheduleEvent<T> {
 	public static final int ADD = 0x0001;
@@ -27,6 +28,11 @@ public class ScheduleEvent<T> {
 	
 	ScheduleEvent(Collection<T> add, Collection<T> rem, int act) {
 		action = validateAction(act);
+		// if action is UPDATE, the collections must have the same number of items, so a 1-to-1 relationship
+		// can be assumed
+		if (action == UPDATE) {
+			validateUpdate(add, rem);
+		}
 		if (add != null) {
 			added.addAll(add);
 		}
@@ -43,6 +49,14 @@ public class ScheduleEvent<T> {
 				return action;
 			default:
 				throw new IllegalArgumentException("Action must be one of ADD, REMOVE, UPDATE.");
+		}
+	}
+	
+	private static void validateUpdate(Collection<?> add, Collection<?> rem) {
+		Objects.requireNonNull(add);
+		Objects.requireNonNull(rem);
+		if (add.size() != rem.size()) {
+			throw new IllegalArgumentException("Update before and after must be same size.");
 		}
 	}
 }
