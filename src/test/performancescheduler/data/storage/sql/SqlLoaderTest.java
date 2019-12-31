81b33d4547dc85f0e77df5bcef93b51540740406
Steven Hudson <shudson6@asu.edu>
2019-12-31 10:34:57 -0500

    set up test suite and test classes for SqlLoader. may need to eventually have test suite to include everything under storage.sql?

---
+++ src/test/performancescheduler/data/storage/sql/SqlLoaderTest.java
@@ -0,0 +1,15 @@
+package performancescheduler.data.storage.sql;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SqlLoaderTest {
+    static DbConnectionService dbcs;
+    
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/test/performancescheduler/data/storage/sql/SqlPerformanceLoaderTest.java
@@ -0,0 +1,14 @@
+package performancescheduler.data.storage.sql;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SqlPerformanceLoaderTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
