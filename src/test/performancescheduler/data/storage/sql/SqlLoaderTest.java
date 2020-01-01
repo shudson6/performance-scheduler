2dad10399cce178940bee02ce67629827d57f58c
Steven Hudson <shudson6@asu.edu>
2020-01-01 09:40:28 -0500

    SqlFeatureLoader tested 100%; removed generated fail() lines from unwritten tests

--- src/test/performancescheduler/data/storage/sql/SqlLoaderTest.java
+++ src/test/performancescheduler/data/storage/sql/SqlLoaderTest.java
@@ -9,7 +9,7 @@ public class SqlLoaderTest {
     
     @Test
     public void test() {
-        fail("Not yet implemented");
+        
     }
 
 }
--- src/test/performancescheduler/data/storage/sql/SqlPerformanceLoaderTest.java
+++ src/test/performancescheduler/data/storage/sql/SqlPerformanceLoaderTest.java
@@ -8,7 +8,7 @@ public class SqlPerformanceLoaderTest {
 
     @Test
     public void test() {
-        fail("Not yet implemented");
+        
     }
 
 }
