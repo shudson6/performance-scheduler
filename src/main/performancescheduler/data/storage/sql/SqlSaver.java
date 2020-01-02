1f2311f2b249e3e3aac29fe612f1c2f9ae7cd8cc
Steven Hudson <shudson6@asu.edu>
2020-01-02 00:05:09 -0500

    added documentation to SqlSaver

--- src/main/performancescheduler/data/storage/sql/SqlSaver.java
+++ src/main/performancescheduler/data/storage/sql/SqlSaver.java
@@ -8,14 +8,35 @@ import java.util.Objects;
 import performancescheduler.data.storage.MetaFeature;
 import performancescheduler.data.storage.MetaPerformance;
 
+/**
+ * Class used by {@link SqlSaver} to save feature and performance data.
+ * 
+ * @author Steven Hudson
+ */
 class SqlSaver {
     private final DbConnectionService dbcs;
     
+    /**
+     * Create a new instance.
+     * 
+     * @param connection the {@link DbConnectionService} which serves this instance
+     */
     public SqlSaver(DbConnectionService connection) {
         Objects.requireNonNull(connection);
         dbcs = connection;
     }
     
+    /**
+     * Saves the provided data to the database. Where elements already exist in the database, determined by checking
+     * UUID, relevant entries are updated. If an element's UUID exists in the database but the instance wraps
+     * {@code null}, indicating the object was "deleted," that entry's {@code active} column is updated to
+     * {@code false}. Either parameter may be {@code null}, however, in such event there may be a risk of exception
+     * e.g. if a performance refers to a feature which does not exist in the database.
+     * 
+     * @param features the features to be saved
+     * @param performances the performances to be saved
+     * @throws IOException if an exception occurs during saving
+     */
     public void save(Collection<MetaFeature> features, Collection<MetaPerformance> performances) throws IOException {
         PsqlDeactivateBuilder delete = new PsqlDeactivateBuilder(dbcs.getProperty("features"),
                 dbcs.getProperty("performances"));
